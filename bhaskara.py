65c3af4ae6b1c3849e929153adf61cd2b6a6f964
JonathanSeverino <jonathandesouza046@gmail.com>
2019-05-27 11:47:43 -0300

    Foi modificado o input do algoritimo e corrigido um erro no tratamento do input, para que não aconteça divisão por zero.

--- bhaskara.py
+++ bhaskara.py
@@ -10,27 +10,29 @@ print("Calculando as raízes da equação de segundo grau.")
 print("Digite os valores de A, B,C")
 
 
-a = float(input('A = '))
-b = float(input('B = '))
-c = float(input('C = '))
+entradas = input("Digite os valores separdos por um espaço: ").split(' ')
+ 
+a = float(entradas [0])
+b = float(entradas [1])
+c = float(entradas [2])
 
 
 #Calculando o delta
 D = delta(a, b, c)
 
 #tratando as saídas
-if D < 0:
+if ((b**2 - 4*a*c) < 0) or (a == 0.0):
 
     print('Não existem raízes reais!')
 
-if D == 0:
+elif D == 0:
 
     unica_raiz = (-b + math.sqrt(D))/(2*a) 
 
     print('Única raiz = {:.2f}'.format(unica_raiz))
 
 
-if D > 0:
+else:
     x_1 = (-b + math.sqrt(D))/(2*a)
     x_2 = (-b - math.sqrt(D))/(2*a)
 
